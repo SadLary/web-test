1409ec8cd8eb545cfd99f4803b88ff57d8277a88
SadLary <lev.shtarov@yandex.ru>
2026-03-31 16:42:55 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+<script setup>
+import { onMounted } from 'vue'
+
+const username = 'Webclient';
+const password = 'Yogi543';
+
+
+onMounted(async () => {
+
+
+  const data = await fetch('/api/restapi/events', {
+  method: 'GET',
+  headers: {
+    Authorization: 'Basic ' + btoa('Webclient:Yogi543'),
+    'Content-Type': 'application/json',
+  },
+});
+  console.log(data.json());
+})
+</script>
+
+<style scoped></style>
---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+
+  // прокси для обхода CORS
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://fencingone.net',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    }
+  }
+})
